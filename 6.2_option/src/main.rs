c889bda454284d62dcd56f4f34dda7b74362a348
Tom Bolton <tbolton96@icloud.com>
2024-06-10 15:29:53 +0100

    Finish section 6.2 match control flow with Option

--- 6.2_option/src/main.rs
+++ 6.2_option/src/main.rs
@@ -9,4 +9,15 @@ fn main() {
     };
 
     println!("x + y is: {sum}");
+
+
+    let dice_roll = 9;
+    match dice_roll {
+        3 => add_fancy_hat(),
+        7 => remove_fancy_hat(),
+        _ => (),
+    }
+
+    fn add_fancy_hat() {}
+    fn remove_fancy_hat() {}
 }
